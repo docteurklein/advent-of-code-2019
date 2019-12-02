dad2f82b4ac6094556e5166a6b7ad08de2c9a799
Klein Florian <florian.klein@free.fr>
2019-12-02 17:44:32 +0100

    day 1: correct answer

--- Day1.hs
+++ Day1.hs
@@ -2,4 +2,4 @@ module Day1 where
 
 day1 input = sum $ map to_fuel $ lines $ input
 
-to_fuel mass = (ceiling $ fromIntegral(read mass::Int) / 3) - 2
+to_fuel mass = (floor $ fromIntegral(read mass::Int) / 3) - 2
