cb4c704d82db810cb273e7e92b0d241c6531985c
Klein Florian <florian.klein@free.fr>
2019-12-02 17:28:55 +0100

    make day1 compile

--- Day1.hs
+++ Day1.hs
@@ -1,7 +1,5 @@
 module Day1 where
 
-import qualified Data.ByteString.Lazy.Char8 as BSC
-
-day1 input = show $ sum $ map to_fuel $ BSC.lines input
+day1 input = sum $ map to_fuel $ lines $ input
 
 to_fuel mass = (ceiling $ fromIntegral(read mass::Int) / 3) - 2
--- Main.hs
+++ Main.hs
@@ -4,10 +4,11 @@ import Network.Wreq
 import Control.Lens
 import System.Environment
 import Data.ByteString.UTF8 (fromString)
+import Data.ByteString.Lazy.UTF8 (toString)
 
 main = do
     cookie <- getEnv "COOKIE"
     let opts = defaults & header "Cookie" .~ [fromString cookie]
     r <- getWith opts "https://adventofcode.com/2019/day/1/input"
     let input = r ^. responseBody
-    putStrLn $ day1 input
+    putStrLn $ show $ day1 $ toString input
