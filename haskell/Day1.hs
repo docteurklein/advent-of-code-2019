c3df7d519467852b64b5183638c2012005844f14
Klein Florian <florian.klein@free.fr>
2019-12-09 14:15:47 +0100

    day1 part 2

--- haskell/Day1.hs
+++ haskell/Day1.hs
@@ -1,5 +1,7 @@
 module Day1 where
 
-day1 input = sum $ map to_fuel $ lines $ input
+day1 input = sum $ map total_fuel $ map (read::String->Int) $ lines input
 
-to_fuel mass = (floor $ fromIntegral(read mass::Int) / 3) - 2
+to_fuel mass = (mass `div` 3) - 2
+
+total_fuel mass = sum $ takeWhile (>=0) $ drop 1 $ iterate to_fuel mass
